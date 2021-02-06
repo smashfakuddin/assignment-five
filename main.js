27063858581afcf3a88721910c51cc14da011472
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-02-06 23:51:11 +0600

    commit

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-const url = ('https://www.themealdb.com/api/json/v1/1/random.php','https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-fetch(url)
-.then(response => response.json())
-.then(data => foodDetect("Apple Frangipan Tart"))
+document.getElementById('search-button').addEventListener('click', function(){
+    const inputName = document.getElementById('meal-input').value;
+    console.log(inputName);
+})
 
-const foodDetect = foodName => {
-    console.log(data[0].meals.strMeal)
+
+
+const url = ('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
+fetch(url)
+    .then(response => response.json())
+    .then(data => foodDetect(data))
+
+const foodDetect = food => {
+    console.log(food.meals);
+    const ul = document.getElementById("foodItem");
+    for (let i = 0; i < food.meals.length; i++) {
+        const foodName = food.meals[i];
+        const li = document.createElement('li');
+        li.innerText =foodName.strMeal;
+        ul.appendChild(li);
+        
+    }
+
+
 }
