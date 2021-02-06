43ddcc880046927936c49529b7600261eedf6d4d
smashfakuddin <smashfakuddin.anthro.cou@gmail.com>
2021-02-07 03:55:29 +0600

    commit

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-document.getElementById('search-button').addEventListener('click', function () {
-    const inputName = document.getElementById('meal-input').value;
-    console.log(inputName);
-})
+// document.getElementById('search-button').addEventListener('click', function(){
+//     const inputName = document.getElementById('meal-input').value; 
+//     //  foodDetect(food); 
+// })
 
+var inputName="";
+function testFunction() {
+    let inputName = document.getElementById('meal-input').value;
+    
+   anotherFunction(inputName);
+   
+};
 
+function anotherFunction(inputName) {
+    let url = (`https://www.themealdb.com/api/json/v1/1/search.php?f=${inputName}`)
+    fetch(url)
+        .then(response => response.json())
+        .then(data => foodDetect(data))
+}
 
-const url = ('https://www.themealdb.com/api/json/v1/1/search.php?f=c')
-fetch(url)
-    .then(response => response.json())
-    .then(data => foodDetect(data))
+// let url = (`https://www.themealdb.com/api/json/v1/1/search.php?f=${testName}`)
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => foodDetect(data))
 
 const foodDetect = food => {
     console.log(food.meals);
